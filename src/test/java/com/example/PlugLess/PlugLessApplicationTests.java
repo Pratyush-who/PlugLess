31b8d32e00eaff153c365b6bac9eb7c06924187e
pratyush-who <pratyushmehra2005@gmail.com>
2026-02-27 07:40:21 +0530

    init

---
+++ src/test/java/com/example/PlugLess/PlugLessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlugLess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlugLessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
